bd9638c3d8d20c71255a1dc4f29854394cde6d7b
Oleksandr Zharkov <baked.zebra.dev@gmail.com>
2021-05-24 20:27:46 +0300

    RWS-10 IT Test update

--- reactive-websockets-test/src/test/java/org/elpis/socket/web/context/BootStarter.java
+++ reactive-websockets-test/src/test/java/org/elpis/socket/web/context/BootStarter.java
@@ -2,18 +2,12 @@ package org.elpis.socket.web.context;
 
 import org.elpis.reactive.websockets.EnableReactiveSocketSecurity;
 import org.elpis.reactive.websockets.EnableReactiveSockets;
+import org.elpis.reactive.websockets.config.annotations.impl.*;
 import org.elpis.reactive.websockets.security.SocketHandshakeService;
 import org.elpis.reactive.websockets.security.principal.Anonymous;
-import org.elpis.reactive.websockets.web.BasicWebSocketResource;
-import org.elpis.reactive.websockets.web.annotations.controller.Outbound;
-import org.elpis.reactive.websockets.web.annotations.controller.SocketResource;
-import org.elpis.reactive.websockets.web.annotations.request.SocketHeader;
-import org.elpis.reactive.websockets.web.annotations.request.SocketPathVariable;
-import org.elpis.reactive.websockets.web.annotations.request.SocketQueryParam;
 import org.elpis.socket.web.context.resource.HeaderSocketResource;
 import org.elpis.socket.web.context.resource.PathVariableSocketResource;
 import org.elpis.socket.web.context.resource.QueryParamSocketResource;
-import org.reactivestreams.Publisher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -22,19 +16,21 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.reactive.socket.server.RequestUpgradeStrategy;
-import org.springframework.web.reactive.socket.server.upgrade.ReactorNettyRequestUpgradeStrategy;
 import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @EnableReactiveSockets
-@Import({HeaderSocketResource.class, PathVariableSocketResource.class, QueryParamSocketResource.class})
+@Import({
+        HeaderSocketResource.class,
+        PathVariableSocketResource.class,
+        QueryParamSocketResource.class,
+        AuthenticationAnnotationEvaluator.class,
+        HeaderAnnotationEvaluator.class,
+        MessageBodyAnnotationEvaluator.class,
+        PathVariableAnnotationEvaluator.class,
+        QueryParameterAnnotationEvaluator.class
+})
 public class BootStarter {
     public static void main(String[] args) {
         SpringApplication.run(BootStarter.class, args);
