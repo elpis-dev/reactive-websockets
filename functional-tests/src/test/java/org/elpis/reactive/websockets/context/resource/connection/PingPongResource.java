95c64a8df140abefb265d4d394169b7f1e411c9d
Oleksandr Zharkov (CW) <Oleksandr.Zharkov@blueyonder.com>
2024-07-05 12:15:45 +0300

    [RWS-30] Added tests for close and ping

--- functional-tests/src/test/java/org/elpis/reactive/websockets/context/resource/connection/PingPongResource.java
+++ functional-tests/src/test/java/org/elpis/reactive/websockets/context/resource/connection/PingPongResource.java
@@ -4,15 +4,11 @@ import org.elpis.reactive.websockets.config.model.Mode;
 import org.elpis.reactive.websockets.web.annotation.PingPong;
 import org.elpis.reactive.websockets.web.annotation.SocketController;
 import org.elpis.reactive.websockets.web.annotation.SocketMapping;
-import org.reactivestreams.Publisher;
-import org.springframework.web.reactive.socket.CloseStatus;
-import reactor.core.publisher.Flux;
-
-import java.time.Duration;
 
 @SocketController("/connection")
 public class PingPongResource {
     @SocketMapping(value = "/ping", mode = Mode.SHARED, pingPong = @PingPong)
     public void ping() {
+        //Empty for test the Ping-Pong
     }
 }
