df11d28ff9931cb53c6b13baa757d4afc4ea66fa
j.fry <baked.zebra.dev@gmail.com>
2025-12-10 19:28:15 +0200

    [RWS-48] chore: Add tests for JsonMapper error handling

--- functional-tests/src/test/java/io/github/elpis/reactive/websockets/impl/util/JsonMapperTest.java
+++ functional-tests/src/test/java/io/github/elpis/reactive/websockets/impl/util/JsonMapperTest.java
@@ -1,7 +1,9 @@
 package io.github.elpis.reactive.websockets.impl.util;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
 import io.github.elpis.reactive.websockets.mapper.JsonMapper;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
@@ -28,5 +30,50 @@ public class JsonMapperTest {
     final Flux<String> result = JsonMapper.applyWithFlux(Map.of("test", "test"));
 
     assertThat(result.blockFirst()).isEqualTo("{\"test\":\"test\"}");
+  }
+
+  @Test
+  public void applyWithFallbackThrowsExceptionWhenCannotProcess() {
+    // Create an object that cannot be serialized (e.g., object with circular reference)
+    final UnserializableObject unserializable = new UnserializableObject();
+
+    assertThatThrownBy(() -> JsonMapper.applyWithFallback(unserializable))
+        .isInstanceOf(RuntimeJsonMappingException.class)
+        .hasMessageContaining("Unable to translate")
+        .hasMessageContaining("UnserializableObject");
+  }
+
+  @Test
+  public void applyWithDefaultReturnsDefaultValueWhenCannotProcess() {
+    final UnserializableObject unserializable = new UnserializableObject();
+    final String defaultValue = "DEFAULT_VALUE";
+
+    final String result = JsonMapper.applyWithDefault(unserializable, defaultValue);
+
+    assertThat(result).isEqualTo(defaultValue);
+  }
+
+  @Test
+  public void applyWithMonoReturnsEmptyWhenCannotProcess() {
+    final UnserializableObject unserializable = new UnserializableObject();
+
+    final Mono<String> result = JsonMapper.applyWithMono(unserializable);
+
+    assertThat(result.blockOptional()).isEmpty();
+  }
+
+  @Test
+  public void applyWithFluxReturnsEmptyWhenCannotProcess() {
+    final UnserializableObject unserializable = new UnserializableObject();
+
+    final Flux<String> result = JsonMapper.applyWithFlux(unserializable);
+
+    assertThat(result.collectList().block()).isEmpty();
+  }
+
+  /** Test class that cannot be serialized by Jackson ObjectMapper. */
+  private static class UnserializableObject {
+    // Adding a field with a circular reference or non-serializable type
+    private final UnserializableObject self = this;
   }
 }
